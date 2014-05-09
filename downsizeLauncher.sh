0590fe8c29e7c98248ceb9bc57c9dbf3df2ea3dd
Sam Rendall <sdrendall@gmail.com>
2014-05-09 16:45:35 -0400

    new method for joining arrays

--- downsizeLauncher.sh
+++ downsizeLauncher.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
+function join { local IFS="$1"; shift; echo "$*"; }
 
 for mouse in `find ~/sr235/ccValidation_03-18-14_croppedHemispheres_tifs -mindepth 2 -type d`
 do
     ims=`find $mouse -type f -name "*.tif"`
-    imPaths="\{\'$(echo ${ims[*]} | tr ' ' "\',\'")\'\}"
+    imPaths=\{\'$(join \',\' $ims)\'\}
     echo $imPaths
     logName=`echo $mouse | cut -d '/' -f 7`
     bsub -q short -W 3:00 -e ~/jobLogs/$logName.err -o ~/jobLogs/$logName.log -R "rusage[mem=16000]" matlab -nosplash -nojvm -r "downsizeTiffs($imPaths)"
