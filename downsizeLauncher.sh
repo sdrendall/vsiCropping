a51f74707a801003fa39d19e4965ff4a2cacc866
Sam Rendall <sdrendall@gmail.com>
2014-05-09 17:47:25 -0400

    new cat method for imPaths

--- downsizeLauncher.sh
+++ downsizeLauncher.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
-function join { local IFS="$1"; shift; echo "$*"; }
-
 for mouse in `find ~/sr235/ccValidation_03-18-14_croppedHemispheres_tifs -mindepth 2 -type d`
 do
     ims=`find $mouse -type f -name "*.tif"`
-    IFS=\',\'
-    imPaths=\{\'${ims[*]}\'\}
-    echo imPaths
+    save_IFS=$IFS
+    IFS="}','{"
+    imPaths=${ims[*]}
+    IFS=$save_IFS
     logName=`echo $mouse | cut -d '/' -f 7`
-    bsub -q short -W 3:00 -e ~/jobLogs/$logName.err -o ~/jobLogs/$logName.log -R "rusage[mem=16000]" matlab -nosplash -nojvm -r "downsizeTiffs($imPaths)"
+    bsub -q short -W 3:00 -e ~/jobLogs/$logName.err -o ~/jobLogs/$logName.log -R "rusage[mem=16000]" matlab -nosplash -nojvm -r "downsizeTiffs({'$imPaths'})"
 done
